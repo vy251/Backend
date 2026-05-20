e495f7f1da1c89fd5caedb7f04e1b290e56c5443
Vishal Yadav <vy1370382@gmail.com>
2026-05-20 16:27:25 +0530

    Implement user authentication with JWT and add middleware for token verification

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,35 +47,39 @@ const userSchema=new Schema({
     }
 },{timestamps:true})
 
-userSchema.pre("Save",async function(next){
-   if(!this.isModified("password")) return next();
-   this.password=await bcrypt.hash(this.password,10);
-   next();
-})
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
 
-userSchema.methods.isPasswordCorrect=async function(passoword){
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect=async function(password){
    return await bcrypt.compare(password,this.password)
 }
 
 userSchema.methods.generateAccessToken=function(){
-    return jwt.sign({
+    return jwt.sign(
+    {
        _id:this._id,
        email:this.email,
        username:this.username,
        fullname:this.fullname
-    }),
+    },
     process.env.ACCESS_TOKEN_SECRET,
     {
         expiresIn:process.env.ACCESS_TOKEN_EXPIRY
     }
+   );
 }
 userSchema.methods.generateRefreshToken=function(){
-    return jwt.sign({
+    return jwt.sign(
+    {
        _id:this._id,
-    }),
+    },
     process.env.REFRESH_TOKEN_SECRET,
     {
         expiresIn:process.env.REFRESH_TOKEN_EXPIRY
     }
+  );
 }
 export const User=mongoose.model("User",userSchema);
