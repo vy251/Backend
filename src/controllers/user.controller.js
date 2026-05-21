f6fe2c88ef55066692684e1f0d33fad417f2c50b
Vishal Yadav <vy1370382@gmail.com>
2026-05-21 19:54:38 +0530

    Add user account management features including password change, account update, and user profile retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+
 // console.log("CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
 // console.log("API_KEY:", process.env.CLOUDINARY_API_KEY);
 // console.log("API_SECRET:", process.env.CLOUDINARY_API_SECRET);
@@ -193,16 +194,16 @@ const refreshAccessToken=asyncHandler(async (req,res)=>{
             secure:true
         }
     
-        const {accessToken,newrefreshToken}=await generateAccessAndRefreshTokens(user._id)
+        const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)
     
         return res
         .status(200)
         .cookie("accessToken",accessToken,options)
-        .cookie("refreshToken",newrefreshToken,options)
+        .cookie("refreshToken",refreshToken,options)
         .json(
             new ApiResponse(
                 200,
-                {accessToken,refreshToken:newrefreshToken},
+                {accessToken,refreshToken},
                 "Access token refreshed"
             )
         )
@@ -210,4 +211,171 @@ const refreshAccessToken=asyncHandler(async (req,res)=>{
         throw new ApiError(401,error?.message || "Invalid refresh token")
     }
 })
-export {registerUser,loginUser,logoutUser,refreshAccessToken};
+
+const changeCurrentPassword=asyncHandler(async(req,res)=>{
+      const {oldPassword,newPassword}=req.body;
+      
+      const user=await User.findById(req.user?._id)
+      const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+      
+      if(!isPasswordCorrect){
+        throw new ApiError(400,"Old password is incorrect");
+      }
+
+      user.password=newPassword;
+      await user.save({validateBeforeSave:false})
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200,{},"Password changed successfully"))
+})
+
+const getCurrentUser=asyncHandler(async(req,res)=>{
+      return res
+      .status(200)
+      .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
+})
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+      const {fullname,email}=req.body;
+
+      if(!fullname || !email){
+        throw new ApiError(400,"fullname and email are required");
+      }
+
+      const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullname,
+                email
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+
+const updateUserAvatar=asyncHandler(async(req,res)=>{
+      const avatarLocalPath=req.file?.path
+
+      if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required")
+      }
+
+      const avatar=await uploadOnCloudinary(avatarLocalPath);
+
+      if(!avatar.url){
+        throw new ApiError(500,"Something went wrong while uploading the avatar");
+      }
+
+      const useravatar=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}
+      ).select("-password")
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200,useravatar,"Avatar updated successfully"))
+})
+
+const updateUserCoverImage=asyncHandler(async(req,res)=>{
+      const coverImagePath=req.file?.path
+
+      if(!coverImagePath){
+        throw new ApiError(400,"Cover image file is required")
+      }
+
+      const coverImage=await uploadOnCloudinary(coverImagePath);
+
+      if(!coverImage.url){
+        throw new ApiError(500,"Something went wrong while uploading the cover image");
+      }
+
+      const usercoverimage=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },
+        {new:true}
+      ).select("-password")
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200,usercoverimage,"Cover image updated successfully"))
+})
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+      const {username}=req.params;
+
+      if(!username?.trim()){
+        throw new ApiError(400,"Username is required");
+      }
+
+      const channel=await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{$size:"$subscribers"},
+                subscribedToCount:{$size:"$subscribedTo"},
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },{
+            $project:{
+                fullname:1,
+                username:1,
+                subscribersCount:1,
+                subscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+      ])
+      
+      if(!channel?.length){
+         throw new ApiError(404,"Channel not found");
+      }
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200,channel[0],"User channel fetched successfully"))
+})
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateUserAvatar,updateUserCoverImage,updateAccountDetails,getUserChannelProfile};
